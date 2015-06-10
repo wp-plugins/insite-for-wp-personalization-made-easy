828c684ca90b0fd6c931f371cb5ef541bc8720e3
duda_danny <duda_danny@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-10 12:51:18 +0000

    tag 1.4.0
    git-svn-id: https://plugins.svn.wordpress.org/insite-for-wp-personalization-made-easy/tags/1.4.0@1178212 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config/config-dev-example.php
@@ -0,0 +1,7 @@
+<?php
+
+$insiteConfig = array(
+    'insiteAPIServerBase' => 'http://localhost:8080',
+    'insiteUIServerBase' => '//localhost:8080',
+    'insiteRTScriptBase' => '//s3.amazonaws.com/insite-sites-test'
+);
